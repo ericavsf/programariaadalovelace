1af4f0a68b6076ae18577b969ddc43538f845854
Erica Ferreira <83288447+ericavsf@users.noreply.github.com>
2021-06-30 17:05:55 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("botaoEnviar").addeventlistener("click",validaFormulario )
+
+function validaFormulario(){
+  if (document.getElementById("nome").value != "" && document.getElementById("email").value != "")
+  {
+   alert("Prontinho! Você receberá as novidade por email")
+  }
+ else
+  {
+   alert("Por favor, preencha os campos nome e email!")
+  }
+}
